7efc1be185dd59f3367b06bf262e7554c57d548e
ValentinSaraiva-MMI <valentin.saraiva@edu.univ-fcomte.fr>
2024-02-21 11:39:09 +0100

    directive and event

--- CintooTutorial1/src/App.vue
+++ CintooTutorial1/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import MyCounter from './components/MyCounter.vue'
 </script>
 
 <template>
@@ -9,6 +10,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
+      <MyCounter />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
---
+++ CintooTutorial1/src/components/MyCounter.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+
+let counter = ref(0)
+let test = ref(0)
+</script>
+
+<template>
+  <div>
+    <button @click="counter++, test++">Current count = {{ counter }}</button>
+    <div>
+      <p class="superior" v-if="counter > 10">Counter is superior to 10</p>
+      <p class="inferior" v-else>counter is inferior to 10</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.superior {
+  color: red;
+}
+
+.inferior {
+  color: blue;
+}
+</style>
